d4e816349d1874e334607da38d2cb5747fb0f2b6
Kyle Geske <stungeye@gmail.com>
2024-10-09 16:04:57 -0500

    Fully implemented the grid on construction and the spawning.

--- Source/BlockHeadCpp24/Private/GridSpawner.cpp
+++ Source/BlockHeadCpp24/Private/GridSpawner.cpp
@@ -2,26 +2,56 @@
 
 
 #include "GridSpawner.h"
+#include "Components/InstancedStaticMeshComponent.h"
 
 // Sets default values
 AGridSpawner::AGridSpawner()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+ 	// This actor does not tick:
+	PrimaryActorTick.bCanEverTick = false;
 
+	InstancedStaticMeshComponent = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("Grid Instanced Mesh"));
+	InstancedStaticMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
 void AGridSpawner::BeginPlay()
 {
 	Super::BeginPlay();
+	GetWorldTimerManager().SetTimer(SpawnTimerHandle, this, &AGridSpawner::SpawnActor, DelayBetweenSpawns, true);
+}
+
+void AGridSpawner::OnConstruction(const FTransform& Transform)
+{
+	if (InstancedStaticMeshComponent->GetStaticMesh()) {
+		InstancedStaticMeshComponent->ClearInstances();
+
+		for (int32 X = 0; X < GridSize; X++) {
+			for (int32 Y = 0; Y < GridSize; Y++) {
+
+				FVector Location{ X * GridSpacing, Y * GridSpacing, 0 };
+				InstancedStaticMeshComponent->AddInstance(FTransform{ Location });
+				UE_LOG(LogTemp, Warning, TEXT("Instance count: %d"), InstancedStaticMeshComponent->GetInstanceCount());
+				UE_LOG(LogTemp, Warning, TEXT("Location: %s"), *Location.ToString());
+			}
+		}
+	}
+}
+
+void AGridSpawner::SpawnActor()
+{
+	if (ActorToSpawn && SpawnActorCount < MaxSpawnedActorCount ) {
+		UE_LOG(LogTemp, Warning, TEXT("SpawnActor"));
+		SpawnActorCount++;
+		FBoxSphereBounds Bounds = InstancedStaticMeshComponent->CalcBounds(InstancedStaticMeshComponent->GetComponentTransform());
+		FVector Location = FMath::RandPointInBox(Bounds.GetBox());
+		GetWorld()->SpawnActor<AActor>(ActorToSpawn, Location, FRotator::ZeroRotator);
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Timer Cleared"));
+		GetWorldTimerManager().ClearTimer(SpawnTimerHandle);
+	}
 	
 }
 
-// Called every frame
-void AGridSpawner::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
 
--- Source/BlockHeadCpp24/Public/GridSpawner.h
+++ Source/BlockHeadCpp24/Public/GridSpawner.h
@@ -6,21 +6,44 @@
 #include "GameFramework/Actor.h"
 #include "GridSpawner.generated.h"
 
+// Forward Declaration. We will include the header in the cpp file.
+class UInstancedStaticMeshComponent;
+
 UCLASS()
 class BLOCKHEADCPP24_API AGridSpawner : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AGridSpawner();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UInstancedStaticMeshComponent> InstancedStaticMeshComponent;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Grid")
+	int32 GridSize{ 10 };
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Grid")
+	double GridSpacing{ 100.0 };
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawning")
+	double DelayBetweenSpawns{ 1.0 };
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawning")
+	int32 MaxSpawnedActorCount{ 100 };
+	
+private:
+	void SpawnActor();
+
+	int32 SpawnActorCount{ 0 };
+	FTimerHandle SpawnTimerHandle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawning", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<AActor> ActorToSpawn;
 };
